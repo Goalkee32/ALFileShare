8b784b901da041ffec75374a73c2d29956e9ab13
Goalkee32 <goalkee32@gmail.com>
2024-11-30 11:44:46 +0100

    Löste problem med upload.php och logged-in.php.

--- upload.php
+++ upload.php
@@ -16,86 +16,54 @@
 
 <main>
     <nav class="w3-bar w3-black">
-        <a href="../upload.php" class="w3-button w3-bar-item">Ladda upp</a>
-        <?php
-        $dir = 'media';
-
-        if (is_dir($dir) && $handle = opendir($dir)) {
-            while (($folder = readdir($handle)) !== false) {
-                if ($folder != '.' && $folder != '..' && is_dir($dir . '/' . $folder)) {
-                    echo '<a href="?dir=' . urlencode($folder) . '" class="w3-button w3-bar-item">' . htmlspecialchars($folder) . '</a>';
-                }
-            }
-            closedir($handle);
-        }
-        ?>
+        <a href="../public/logged-in.php" class="w3-button w3-bar-item">Till dina mappar</a>
     </nav>
 
     <nav>
         <h2>Filuppladdning och Mapphantering</h2>
 
         <?php
-        $baseDir = 'media/';
-        $currentDir = $baseDir;
-
+        $baseDir = 'media';
+        $currentDir = realpath($baseDir);
         if (isset($_GET['dir'])) {
-            $currentDir = realpath($baseDir . '/' . $_GET['dir']);
-            if (strpos($currentDir, realpath($baseDir)) !== 0) {
-                $currentDir = $baseDir;
+            $requestedDir = realpath($baseDir . '/' . basename($_GET['dir']));
+            if ($requestedDir && strpos($requestedDir, realpath($baseDir)) === 0) {
+                $currentDir = $requestedDir;
             }
         }
-
         echo "<h3>Nuvarande mapp: " . htmlspecialchars(str_replace(realpath($baseDir), '', $currentDir)) . "</h3>";
         ?>
 
+        <!-- Skapa ny mapp -->
         <form action="upload.php" method="post">
             <input type="text" name="newFolderName" placeholder="Ange nytt mappnamn" required>
             <input type="hidden" name="currentDir" value="<?php echo htmlspecialchars($currentDir); ?>">
             <input type="submit" name="createFolder" value="Skapa mapp">
         </form>
 
+        <!-- Ladda upp fil -->
         <form action="upload.php" method="post" enctype="multipart/form-data">
             <input type="file" name="uploadedFile" required>
             <input type="hidden" name="currentDir" value="<?php echo htmlspecialchars($currentDir); ?>">
             <input type="submit" value="Ladda upp fil">
         </form>
 
+        <!-- Lista filer och mappar -->
         <?php
         $items = scandir($currentDir);
         echo "<ul>";
         foreach ($items as $item) {
             if ($item === '.' || $item === '..') continue;
-
             $itemPath = $currentDir . '/' . $item;
-            $relativePath = str_replace(realpath($baseDir), '', realpath($itemPath));
+            $relativePath = str_replace(realpath($baseDir), '', $itemPath);
 
             if (is_dir($itemPath)) {
-                echo "<li><a href='?dir=" . urlencode($relativePath) . "'>[Mapp] " . htmlspecialchars($item) . "</a></li>";
+                echo "<li><a href='?dir=" . urlencode(basename($relativePath)) . "'>[Mapp] " . htmlspecialchars($item) . "</a></li>";
             } else {
                 echo "<li>" . htmlspecialchars($item) . "</li>";
             }
         }
         echo "</ul>";
-
-        // Visa bilder
-        $imageExtensions = ['jpg', 'jpeg', 'png', 'gif'];
-        $images = array_filter($items, function($item) use ($imageExtensions, $currentDir) {
-            $ext = pathinfo($item, PATHINFO_EXTENSION);
-            return in_array(strtolower($ext), $imageExtensions) && is_file($currentDir . '/' . $item);
-        });
-
-        if (!empty($images)) {
-            echo '<div class="w3-row-padding">';
-            foreach ($images as $image) {
-                $imagePath = str_replace(realpath($_SERVER['DOCUMENT_ROOT']), '', realpath($currentDir . '/' . $image));
-                echo '<div class="w3-col s4">';
-                echo '<img src="' . $imagePath . '" alt="' . htmlspecialchars($image) . '" style="width:100%" class="w3-hover-opacity">';
-                echo '</div>';
-            }
-            echo '</div>';
-        } else {
-            echo "<p>Inga bilder hittades i denna mapp.</p>";
-        }
         ?>
     </nav>
 </main>
